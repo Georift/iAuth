3512a30b34764ab85d0342e1e4cd4bf545034423
Tim <php-tacroy@live.com>
2011-06-30 17:36:46 +0800

    Adding Sessions to auth system with the addition of a 255 long hash. Added a new database table app_sessions.

--- auth.php
+++ auth.php
@@ -5,6 +5,7 @@ include("includes/mysql_connect.php");
 $user = mysql_real_escape_string($_GET['user']);
 $pass = md5($_GET['pass']);
 
+// Ban checking
 // Check make sure the user has no previous bans.
 // If they do alert them of it.
 $get_banned = mysql_query("SELECT expires FROM bans WHERE ip = '".$_SERVER['REMOTE_ADDR']."' AND expires >= '".time()."' AND exception = '0'") or die(mysql_error());
@@ -14,53 +15,72 @@ if (mysql_num_rows($get_banned) >= 1){
 	die("Your IP has been banned and will be unbanned in ".date("i:s", $time_left)." minutes.");	
 }
 
-// The user has not been marked before.
+// The user has not been marked as banned before.
 // Try the login.
 if ($user == "" OR $pass == ""){
 	// Output failed if the operation
 	// was not completed sucsessfully.
 	echo "FAILED";	
 }else{
-	// Load the users licence.
-	// Since we only need to know the application
-	// id we don't need to select much for now.
-	$run = mysql_query("SELECT aid,id FROM licences WHERE user = '".$user."' AND pass = '".$pass."'");
-	// Set the time to check for any previous bans
-	// Check for the bans
-	$last_time = time() - 3600;
-	$failed = mysql_query("SELECT * FROM fail_log WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."' AND type = '2' AND counted = '0'") or die(mysql_error());
-	// Limit the allowance for failed entry's
-	// to three. If they pass that add there
-	// IP address to the bans table.
-	if (mysql_num_rows($failed) >= 3){
-		$new_time = time() + 3600;
-		mysql_query("INSERT INTO bans(ip, time, expires) VALUES('".$_SERVER['REMOTE_ADDR']."','".time()."','".$new_time."')") or die(mysql_error());
-		$last_time = time() - 3600;
-		mysql_query("UPDATE fail_log SET counted = '1' WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."' AND type = '2'");
-		die("You have been locked out for an hour. Please try again later.");
-	}
-	// Check if the username and password is valid
-	// If not then store the failed attempt in fail_log
-	// Once three are reached counted will be changed to 1
-	// and they will be disregarded.
-	if (mysql_num_rows($run)==0){
-		echo "FAILED";	
-		mysql_query("INSERT INTO fail_log(time,ip,type) VALUES('".time()."','".$_SERVER['REMOTE_ADDR']."','2')");
+	// check if the user has any outstanding sessions before trying to make a new one.
+	$get_existing = mysql_query("SELECT * FROM app_sessions WHERE ip = '".$_SERVER['REMOTE_ADDR']."' AND expires <= '".time()."'") or die(mysql_error());
+	if (mysql_num_rows($get_existing) >= 1){
+		// The user has a previous session.
+		// TODO: Add plugin hook here;
+			
 	}else{
-		// User has passes the authentication process.
-		// We can share any information needed now.
-		echo "ALLOWED";
-		$aid = mysql_fetch_row($run);
+		// Load the users licence.
+		// Since we only need to know the application
+		// id we don't need to select much for now.
+		$run = mysql_query("SELECT aid,id FROM licences WHERE user = '".$user."' AND pass = '".$pass."'");
+		// Set the time to check for any previous bans
+		// Check for the bans
 		$last_time = time() - 3600;
-		// Log the access if the last access by this IP was an hour ago.
-		$get_last = mysql_query("SELECT * FROM access_log WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."'");
-		if (mysql_num_rows($get_last) != 1){
-			if (isset($_GET['a']) == false){	
-				// Get the licence ID and save it to database.
-				//$get_licence = mysql_query("SELECT id FROM licence");
-				mysql_query("INSERT INTO access_log (time ,aid ,ip, lid) VALUES ('".time()."','".$aid[0]."','".$_SERVER['REMOTE_ADDR']."', '".$aid[1]."')") or die(mysql_error());
-			}
+		$failed = mysql_query("SELECT * FROM fail_log WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."' AND type = '2' AND counted = '0'") or die(mysql_error());
+		// Limit the allowance for failed entry's
+		// to three. If they pass that add there
+		// IP address to the bans table.
+		if (mysql_num_rows($failed) >= 3){
+			$new_time = time() + 3600;
+			mysql_query("INSERT INTO bans(ip, time, expires) VALUES('".$_SERVER['REMOTE_ADDR']."','".time()."','".$new_time."')") or die(mysql_error());
+			$last_time = time() - 3600;
+			mysql_query("UPDATE fail_log SET counted = '1' WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."' AND type = '2'");
+			die("You have been locked out for an hour. Please try again later.");
 		}
+		// Check if the username and password is valid
+		// If not then store the failed attempt in fail_log
+		// Once three are reached counted will be changed to 1
+		// and they will be disregarded.
+		if (mysql_num_rows($run)==0){
+			echo "FAILED";	
+			mysql_query("INSERT INTO fail_log(time,ip,type) VALUES('".time()."','".$_SERVER['REMOTE_ADDR']."','2')");
+		}else{
+			// User has passes the authentication process.
+			// We can share any information needed now.
+			//echo "ALLOWED";
+			$aid = mysql_fetch_row($run);
+			$last_time = time() - 3600;
+			// Log the access if the last access by this IP was an hour ago.
+			$get_last = mysql_query("SELECT * FROM access_log WHERE time >= '".$last_time."' AND ip = '".$_SERVER['REMOTE_ADDR']."'");
+			if (mysql_num_rows($get_last) != 1){
+				if (isset($_GET['a']) == false){	
+					// Get the licence ID and save it to database.
+					//$get_licence = mysql_query("SELECT id FROM licence");
+					mysql_query("INSERT INTO access_log (time, aid, ip, lid) VALUES ('".time()."','".$aid[0]."','".$_SERVER['REMOTE_ADDR']."', '".$aid[1]."')") or die(mysql_error());
+				}
+			}
+			// Insert the session into a database with the last request time.
+			// This key will be valid for a total of 60 minutes
+			// 255 characters key to be safe.
+			$key = "";
+			$charset = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+			for ($i = 1; $i<=255; $i++){
+				$rand_char = mb_substr($charset, rand() % strlen($charset), 1);
+				$key .= $rand_char;
+			}	
+			mysql_query("INSERT INTO app_sessions(ip, lid, hash, expires) VALUES('".$_SERVER['REMOTE_ADDR']."','".$aid[1]."','".$key."','".(time() - 3600)."')") or die(mysql_error());
+			echo $key;
+		}	
 	}
 }
 ?>
